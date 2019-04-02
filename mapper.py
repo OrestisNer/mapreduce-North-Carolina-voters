a8c9314d600a182436072a3e7a81f692cccdcf0a
student <dai16019@uom.edu.gr>
2019-04-02 16:53:59 +0300

    mapper without combiner

--- mapper.py
+++ mapper.py
@@ -22,19 +22,19 @@ for line in input_stream:
 
     key = line[9] + line[10] + line[11] + line[14]
     value = line[68]
-
+	print('%s\t%s' % (key, value))
         #case of duplicate
-    if key in myDictionary:
-        myDictionary[key] = myDictionary[key] + " " + value
-    else:
+    #if key in myDictionary:
+        #myDictionary[key] = myDictionary[key] + " " + value
+    #else:
         #case of unique
-        myDictionary[key] = value
+        #myDictionary[key] = value
         #when dictionary length is greater than thresh then print
-    if (int(len(myDictionary)) > threshold):
-        for k, v in myDictionary.items():
-            print('%s\t%s' % (k, v))
+    #if (int(len(myDictionary)) > threshold):
+        #for k, v in myDictionary.items():
+            #print('%s\t%s' % (k, v))
             #cleans up dictionary
-        myDictionary.clear()
+        #myDictionary.clear()
 #reading ends but our dictionary might have items so we print!
-for k, v in myDictionary.items():
-    print('%s\t%s' % (k, v))
+#for k, v in myDictionary.items():
+    #print('%s\t%s' % (k, v))
