f1f730ee0401a7ea54f462a7a08ccc5d8e004df3
unlucky_coder <dai16019@uom.edu.gr>
2019-04-03 18:21:57 +0300

    comments and small changes

--- mapper.py
+++ mapper.py
@@ -4,7 +4,9 @@
 import sys
 import re
 import io
+# myDictionary stores our data in key value form
 myDictionary = {}
+#threshold = max number of items that myDictionary can have before printing
 threshold = 5000
 # input comes from STDIN (standard input)
 input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='windows-1252')
@@ -22,19 +24,21 @@ for line in input_stream:
 
     key = line[9] + line[10] + line[11] + line[14]
     value = line[68]
-    print('%s\t%s' % (key, value))
-        #case of duplicate
+	
+    #case of duplicate
     if key in myDictionary:
         myDictionary[key] = myDictionary[key] + " " + value
-    else:
-        #case of unique
+    #case of unique
+	else:
         myDictionary[key] = value
-        #when dictionary length is greater than thresh then print
+        
+	#when dictionary length is greater than threshold then print
     if (int(len(myDictionary)) > threshold):
         for k, v in myDictionary.items():
             print('%s\t%s' % (k, v))
-            #cleans up dictionary
+        #cleans up dictionary
         myDictionary.clear()
+		
 #reading ends but our dictionary might have items so we print!
 for k, v in myDictionary.items():
     print('%s\t%s' % (k, v))
--- reducer.py
+++ reducer.py
@@ -11,25 +11,26 @@ value=None
 
 #input comes from STDIN
 for line in sys.stdin:
-    #key, value = line.strip().split('\t')
-    #print(key+"-"+value)
     l=line.strip();
     key, value = l.split('\t',1)
     if current_key == key:
         current_value=current_value+" "+value
         i=i+1
     else:
-        if current_key and i>0:
+        if i>0 and current_key:
             print(current_key+"\t"+current_value)
             i=0
-        elif current_key and (' ' in current_value):
+        elif (' ' in current_value) and current_key:
             print(current_key+"\t"+current_value)
             i=0
         current_value = value
         current_key = key
+		
 #prints last input in case it's a dublicate
+#case i>0
 if(i>0):
     print(current_key+"\t"+current_value)
-#elif(' ' in current_value):
-	#print(current_key+"\t"+current_value)
+#case the entry was already combined (mapper)
+elif(' ' in current_value):
+    print(current_key+"\t"+current_value)
 
