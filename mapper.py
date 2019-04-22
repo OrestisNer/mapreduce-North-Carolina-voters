382e40686664fc65ad2ab2a3d93bea7aae665a3c
Orestis <orestis@ep-mail.gr>
2019-04-22 10:17:23 +0300

    Add annotiations and tab consistency

--- mapper.py
+++ mapper.py
@@ -1,44 +1,42 @@
 #!/usr/bin/env python3
-
 """mapper.py"""
 import sys
 import re
 import io
-# myDictionary stores our data in key value form
-myDictionary = {}
-#threshold = max number of items that myDictionary can have before printing
+# outputDict stores our data in key value form
+outputDict = {}
+# threshold = max number of items that outputDict can have before printing
 threshold = 5000
 # input comes from STDIN (standard input)
+# changing encoding to read non UTF-8 characters
 input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='windows-1252')
 
 for line in input_stream:
+    # regex to find all values into quotes (e.g "word")
     line = re.findall("\"[^\"]*\"", line)
-    # we already know which columns we need
-    # first index = 0
-    # index 9 = "last_name"
-    # index 10 = "first_name"
-    # index 11 = "middle_name"
-
-    # index 14 = "res_city_desc"
-    # index 68 = "NCID"
-
+    """
+    We already know which columns we need.
+    index 9 = "last_name", index 10 = "first_name"
+    index 11 = "middle_name", index 14 = "res_city_desc"
+    index 68 = "NCID".
+    """
     key = line[9] + line[10] + line[11] + line[14]
     value = line[68]
-	
-    #case of duplicate
-    if key in myDictionary:
-        myDictionary[key] = myDictionary[key] + " " + value
-    #case of unique
-	else:
-        myDictionary[key] = value
-        
-	#when dictionary length is greater than threshold then print
-    if (int(len(myDictionary)) > threshold):
-        for k, v in myDictionary.items():
+
+    # case of duplicate
+    if key in outputDict:
+        outputDict[key] = outputDict[key] + " " + value
+    # case of unique
+    else:
+        outputDict[key] = value
+
+    # when dictionary length is greater than threshold then print
+    if (int(len(outputDict)) > threshold):
+        for k, v in outputDict.items():
             print('%s\t%s' % (k, v))
-        #cleans up dictionary
-        myDictionary.clear()
-		
-#reading ends but our dictionary might have items so we print!
-for k, v in myDictionary.items():
+        # cleans up dictionary
+        outputDict.clear()
+
+# reading ends but our dictionary might have items so we print!
+for k, v in outputDict.items():
     print('%s\t%s' % (k, v))
--- reducer.py
+++ reducer.py
@@ -1,36 +1,43 @@
 #!/usr/bin/env python3
 """reducer.py"""
-
 import sys
 
-current_key=None
-current_value=None
-i=0
-key=None
-value=None
+"""we do take advantage of the shuffle merge sort phase of the reducer
+   that's why we use the current <key,value> and compare it with
+   the very next entry, in case that the current key is different
+   than the key we do know that we won't encounter the current key again."""
 
-#input comes from STDIN
+current_key = None
+current_value = None
+dublicate_counter = 0
+key = None
+value = None
+
+# input comes from STDIN
 for line in sys.stdin:
-    l=line.strip();
-    key, value = l.split('\t',1)
+    key, value = line.strip().split('\t', 1)
+    # in this case our entries have the same key thus we got a "duplicate"
     if current_key == key:
-        current_value=current_value+" "+value
-        i=i+1
+        current_value = current_value+" "+value
+        dublicate_counter += 1
     else:
-        if i>0 and current_key:
+        # in this case we encountered a diffrent key but our dulpicate counter is greater than 0
+        # so we print the previous key-value pair
+        if dublicate_counter > 0 and current_key:
             print(current_key+"\t"+current_value)
-            i=0
+            dublicate_counter = 0
+        # in this last case if we hadnt classify the entry as duplicate we check if the mapper did so
+        # in case that the entry has been merged in the mapping phase we do print it
         elif (' ' in current_value) and current_key:
             print(current_key+"\t"+current_value)
-            i=0
+            dublicate_counter = 0
         current_value = value
         current_key = key
-		
-#prints last input in case it's a dublicate
-#case i>0
-if(i>0):
+
+# prints last input in case it's a dublicate
+# case i>0
+if(dublicate_counter > 0):
     print(current_key+"\t"+current_value)
-#case the entry was already combined (mapper)
+# case the entry was already combined (mapper)
 elif(' ' in current_value):
     print(current_key+"\t"+current_value)
-
